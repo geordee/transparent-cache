0f489e745661a21532daedee8d9ad8bcb38a2770
Geordee Naliyath <geordee@gmail.com>
2020-08-01 12:33:19 +0400

    Implement a slow API

--- app.lua
+++ app.lua
@@ -1,8 +1,14 @@
 local lapis = require("lapis")
+local socket = require("socket")
 local app = lapis.Application()
 
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
+end)
+
+app:get("/slow", function()
+  socket.sleep(3)
+  return "Slow API, returns after 3 seconds<br />Generated at " .. os.date("%Y-%m-%d %H:%M:%S")
 end)
 
 return app
